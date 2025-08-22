077aa7e66c91441bf4ef2a16c59c0524adc903ae
Harkins <tmngoc1108@gmail.com>
2025-08-22 23:41:59 +0700

    8. Tutorial: Set up a basic health system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,18 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
+    public float speed = 3f;
     Vector2 move;
+
+    public int maxHealth = 5;
+    int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
         // QualitySettings.vSyncCount = 0; // Disable VSync
         // Application.targetFrameRate = 10; // Set target frame rate to 10 FPS
     }
@@ -29,7 +34,14 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Physics updates can be handled here if needed
-        Vector2 position = (Vector2)transform.position + move * 3f * Time.deltaTime;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        // Mathf.Clamp để giới hạn hp min = 0, max = maxHealth
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
