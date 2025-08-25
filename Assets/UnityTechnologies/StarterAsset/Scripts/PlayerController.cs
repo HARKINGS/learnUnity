b868b883c9f24018bb09a6e954b9fcd38c013e30
Harkins <tmngoc1108@gmail.com>
2025-08-25 17:20:59 +0700

    12: Tutorial: Characters and interaction mechanics

--- Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -62,7 +62,7 @@ public class PlayerController : MonoBehaviour
             damageCooldown = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        StartUIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
     }
 
 }
